4a3ec4960badcc3bee052f61249185a2ad3967d0
Jasmine Akbari <jasmineakbari@Jasmines-MBP.attlocal.net>
2021-07-15 16:30:59 -0400

    added comments and removed unusable code

--- script.py
+++ script.py
@@ -3,9 +3,8 @@ import numpy as np
 import PyPDF2
 import textract
 import re
-# outputs collected data to csv file for readability
-import csv
 
+# File to scrape text from
 filename ='./assets/articles/20.08.07.Blockchain.Lecture1.pdf' 
 
 pdfFileObj = open(filename,'rb')               
@@ -15,29 +14,34 @@ num_pages = pdfReader.numPages
 
 count = 0
 text = ""
-                                                            
+
+# while to loop to cycle through pages
 while count < num_pages:                       
     pageObj = pdfReader.getPage(count)
     count +=1
     text += pageObj.extractText()
-    
+
+# checks to see if words were returned
 if text != "":
     text = text
- 
+
+# converts images to text using textract
 else:
     text = textract.process('http://bit.ly/epo_keyword_extraction_document', method='tesseract', language='eng')
 
 
 print(type(text))
-# commented out for testing
+# commented out for testing #
 # text = text.encode('ascii','ignore').lower()
+#
+
 keywords = re.findall(r'[a-zA-Z]\w+',text)
 
-# data/words collected from the article
+# creates data frame from words pulled from the article
 df = pd.DataFrame(list(set(keywords)),columns=['keywords'])
 
 
-# formatting for data collected
+# formatting/columns for data collected
 def weightage(word,text,number_of_documents=1):
     word_list = re.findall(word,text)
     number_of_times_word_appeared =len(word_list)
@@ -47,7 +51,7 @@ def weightage(word,text,number_of_documents=1):
     return number_of_times_word_appeared,tf,idf ,tf_idf 
 
 
-    
+# calculating term frequency   
 df['number_of_times_word_appeared'] = df['keywords'].apply(lambda x: weightage(x,text)[0])
 df['tf'] = df['keywords'].apply(lambda x: weightage(x,text)[1])
 df['idf'] = df['keywords'].apply(lambda x: weightage(x,text)[2])
@@ -58,10 +62,5 @@ df.head(25)
 
 print(df)
 
-# function to create the csv file from the table data created
-# with open ('article1.cv','wb') as file:
-#    writer = csv.writer(file)
-#    for row in df:
-#         writer.writerow(row)
-
+# function to create the csv file from the dataframe created
 df.to_csv('article_1.csv')
