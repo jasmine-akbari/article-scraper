aa06575b897a77a8f1434bb0b0a19cd184f5669d
Jasmine Akbari <jasmineakbari@Jasmines-MacBook-Pro.local>
2021-07-26 09:20:26 -0400

    comparing gensim to pypdf2 extraction

--- script.py
+++ script.py
@@ -5,6 +5,10 @@ import textract
 import re
 from colorama import init, Fore, Back, Style
 
+from gensim.summarization import keywords
+import warnings
+warnings.filterwarnings("ignore")
+
 
 input(Fore.CYAN + 'Welcome to Amesite\'s Article Scraper! This application will search\nany given PDF File for important Key Words/Topics. \n' + Fore.YELLOW + '(Press Enter to Continue)')
 givenFile = input(Fore.CYAN + 'Please type here the name of the PDF File you would like to search to begin.\n' + Fore.YELLOW + '(Please make sure the PDF File is saved in the same location you saved this application)')
@@ -34,34 +38,40 @@ else:
 
 
 text = text.lower()
-keywords = re.findall(r'[a-zA-Z]\w+',text)
 
-# creates data frame from words pulled from the article
-df = pd.DataFrame(list(set(keywords)),columns=['keywords'])
+values = keywords(text=text,split='\n',scores=True)
 
+data = pd.DataFrame(values,columns=['keyword','score'])
+data = data.sort_values('score',ascending=False)
+df_max_res = data.head(15)
+# keywords = re.findall(r'[a-zA-Z]\w+',text)
 
-# formatting/columns for data collected
-def weightage(word,text,number_of_documents=1):
-    word_list = re.findall(word,text)
-    number_of_times_word_appeared =len(word_list)
-    word_length = len(word) # Finding length of individual words
-    tf = number_of_times_word_appeared/float(len(text))
-    idf = np.log((number_of_documents)/float(number_of_times_word_appeared))
-    tf_idf = tf*idf
-    return word_length,number_of_times_word_appeared,tf,idf ,tf_idf 
+# # creates data frame from words pulled from the article
+# df = pd.DataFrame(list(set(keywords)),columns=['keywords'])
 
 
-# calculating term frequency and importance  
-df['word_length'] = df['keywords'].apply(lambda x: weightage (x,text)[0])
-df['number_of_times_word_appeared'] = df['keywords'].apply(lambda x: weightage(x,text)[0])
-df['tf'] = df['keywords'].apply(lambda x: weightage(x,text)[1])
-df['idf'] = df['keywords'].apply(lambda x: weightage(x,text)[2])
-df['tf_idf'] = df['keywords'].apply(lambda x: weightage(x,text)[3])
+# # formatting/columns for data collected
+# def weightage(word,text,number_of_documents=1):
+#     word_list = re.findall(word,text)
+#     number_of_times_word_appeared =len(word_list)
+#     word_length = len(word) # Finding length of individual words
+#     tf = number_of_times_word_appeared/float(len(text))
+#     idf = np.log((number_of_documents)/float(number_of_times_word_appeared))
+#     tf_idf = tf*idf
+#     return word_length,number_of_times_word_appeared,tf,idf ,tf_idf 
 
 
-df.drop_duplicates(inplace = True)
-df = df.sort_values(['word_length', 'tf_idf'], ascending=False)
-df_max_res = df.head(15)
+# # calculating term frequency and importance  
+# df['word_length'] = df['keywords'].apply(lambda x: weightage (x,text)[0])
+# df['number_of_times_word_appeared'] = df['keywords'].apply(lambda x: weightage(x,text)[0])
+# df['tf'] = df['keywords'].apply(lambda x: weightage(x,text)[1])
+# df['idf'] = df['keywords'].apply(lambda x: weightage(x,text)[2])
+# df['tf_idf'] = df['keywords'].apply(lambda x: weightage(x,text)[3])
+
+
+# df.drop_duplicates(inplace = True)
+# df = df.sort_values(['word_length', 'tf_idf'], ascending=False)
+# df_max_res = df.head(15)
 
 # function to create the csv file from the dataframe created
 df_max_res.to_csv('./dist/article_1.csv')
